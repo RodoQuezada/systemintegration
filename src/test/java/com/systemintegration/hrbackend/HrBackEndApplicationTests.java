f7ccddad440abdfdc6787bc0c8638c3ac29e1a5b
ro <rodolfo.quezada.s@gmail.com>
2019-05-09 18:06:23 -0400

    pom changes. circle ci testing. test 3

--- src/test/java/com/systemintegration/hrbackend/HrBackEndApplicationTests.java
+++ src/test/java/com/systemintegration/hrbackend/HrBackEndApplicationTests.java
@@ -2,6 +2,7 @@ package com.systemintegration.hrbackend;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
